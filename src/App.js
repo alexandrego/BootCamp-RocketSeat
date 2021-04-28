e7a35b317a3acf8507e482830533fa6def1359a0
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2021-04-28 01:19:56 -0300

    Aula de Estado e Imutbilidade Ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-     import React from 'react';
+     import React, { useState } from 'react';
      import Header from './components/Header';
 
      function App() {
-         return <Header />;
+         const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+         function handleAddProject() {
+             //projects.push(`Novo Projeto ${Date.now()}`);
+             setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+             console.log(projects);
+         }
+
+         return ( 
+            <>
+                <Header title="Projects" />
+               
+                <ul>
+                    {projects.map(project => <li key={project}>{project}</li>)}
+                </ul>
+
+                <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+            </>
+         );         
      }
 
      export default App;
