8ab2a55e30d725d71559f62a7819a2f84dd117ef
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2021-04-29 00:50:21 -0300

    chore: Concluida aula de Importação CSS e Imagens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
      import React, { useState } from 'react';
      import Header from './components/Header';
+     import './App.css';
+     import backgroundImage from './assets/eu.jpg';
 
      function App() {
          const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
@@ -14,6 +16,8 @@
          return ( 
             <>
                 <Header title="Projects" />
+
+                <img width={300} src={backgroundImage} />
                
                 <ul>
                     {projects.map(project => <li key={project}>{project}</li>)}
